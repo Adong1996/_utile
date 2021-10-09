7f4f2787b79b0a5bab4f38b37c47f40bbd5d905e
Dong <80949922+Adong1996@users.noreply.github.com>
2021-10-09 13:29:45 +0800

    Update newInstance.js

--- object/newInstance.js
+++ object/newInstance.js
@@ -30,3 +30,24 @@ export function newInstance(Fn, ...args) {
   // 返回新建的对象
   return obj
 }
+
+// new 关键字
+/*
+  创建了一个空对象，作为将要返回的对象实例
+  将这个空对象的原型，指向构造函数的prototype属性
+  将这个空对象赋值给函数内部的this关键字
+  开始执行构造函数内部的代码
+ */
+
+  function _new(constructor, params) {
+    // 将 arguments 对象转为数组
+    var args = [].slice.call(arguments)
+    // 取出构造函数
+    var constructor = arr.shift()
+    // 创建一个空对象，继承构造函数的 prototype 属性
+    var context = Object.create(constructor.prototype)
+     // 执行构造函数 传入剩余参数
+    var result = constructor.apply(context, args)
+     // 如果返回结果是对象，就直接返回，否则返回 context 对象
+    return (typeof result === 'object' && result != null) ? result : context
+  }
